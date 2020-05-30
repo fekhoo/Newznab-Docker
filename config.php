d5f032f657a5ade1da3e9879e4534364c4a39195
fekhoo <44518355+fekhoo@users.noreply.github.com>
2020-05-29 23:53:56 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,32 +1,8 @@
 <?php
-
-//=========================
-// Config you must change - updated by installer.
-//=========================
-define('DB_TYPE', 'mysql');
-define('DB_HOST', 'localhost');
-define('DB_PORT', 3306);
-define('DB_USER', 'root');
-define('DB_PASSWORD','my-secret-pw');
-define('DB_NAME', 'newznab');
-define('DB_INNODB', true);
-define('DB_PCONNECT', true);
-define('DB_ERRORMODE', PDO::ERRMODE_SILENT);
-
-define('NNTP_USERNAME', 'nntp_user');
-define('NNTP_PASSWORD', 'nntp_password');
-define('NNTP_SERVER', 'nntp_server');
-define('NNTP_PORT', '563');
-define('NNTP_SSLENABLED', true);
-
 define('CACHEOPT_METHOD', 'none');
 define('CACHEOPT_TTLFAST', '120');
 define('CACHEOPT_TTLMEDIUM', '600');
 define('CACHEOPT_TTLSLOW', '1800');
 define('CACHEOPT_MEMCACHE_SERVER', '127.0.0.1');
 define('CACHEOPT_MEMCACHE_PORT', '11211');
-
-// define('EXTERNAL_PROXY_IP', '10.35.103.127'); //Internal address of public facing server
-// define('EXTERNAL_HOST_NAME', ''); //The external hostname that should be used
-
 require("automated.config.php");
