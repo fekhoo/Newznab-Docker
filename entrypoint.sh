4b1b50f4eaba24c4ffe10e0052344b4ba664ee89
fekhoo <44518355+fekhoo@users.noreply.github.com>
2020-05-30 00:11:24 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,20 +3,20 @@
 #Creating Config File
 if [[ -n "$DB_TYPE" && -n "$DB_HOST" && -n "$DB_PORT" && -n "$DB_USER" && -n "$DB_PASSWORD" && -n "$DB_NAME" && -n "$NNTP_USERNAME" && -n "$NNTP_PASSWORD" && -n "$NNTP_SERVER" && -n "$NNTP_PORT" && -n "$NNTP_SSLENABLED" ]]; then
 	echo "Creating database configuration"
-	echo "\define('DB_TYPE', '$DB_TYPE');" >> /config/config.php
-	echo "\define('DB_HOST', '$DB_HOST');" >> /config/config.php
-	echo "\define('DB_PORT', $DB_PORT);" >> /config/config.php
-	echo "\define('DB_USER', '$DB_USER');" >> /config/config.php
-	echo "\define('DB_PASSWORD','$DB_PASSWORD');" >> /config/config.php
-	echo "\define('DB_NAME', '$DB_NAME');" >> /config/config.php
-	echo "\define('DB_INNODB', true);" >> /config/config.php
-	echo "\define('DB_PCONNECT', true);" >> /config/config.php
-	echo "\define('DB_ERRORMODE', PDO::ERRMODE_SILENT);" >> /config/config.php
-	echo "\define('NNTP_USERNAME', '$NNTP_USERNAME');" >> /config/config.php
-	echo "\define('NNTP_PASSWORD', '$NNTP_PASSWORD');" >> /config/config.php
-	echo "\define('NNTP_SERVER', '$NNTP_SERVER');" >> /config/config.php
-	echo "\define('NNTP_PORT', '$NNTP_PORT');" >> /config/config.php
-	echo "\define('NNTP_SSLENABLED', $NNTP_SSLENABLED);" >> /config/config.php
+	echo "define('DB_TYPE', '$DB_TYPE');" >> /config/config.php
+	echo "define('DB_HOST', '$DB_HOST');" >> /config/config.php
+	echo "define('DB_PORT', $DB_PORT);" >> /config/config.php
+	echo "define('DB_USER', '$DB_USER');" >> /config/config.php
+	echo "define('DB_PASSWORD','$DB_PASSWORD');" >> /config/config.php
+	echo "define('DB_NAME', '$DB_NAME');" >> /config/config.php
+	echo "define('DB_INNODB', true);" >> /config/config.php
+	echo "define('DB_PCONNECT', true);" >> /config/config.php
+	echo "define('DB_ERRORMODE', PDO::ERRMODE_SILENT);" >> /config/config.php
+	echo "define('NNTP_USERNAME', '$NNTP_USERNAME');" >> /config/config.php
+	echo "define('NNTP_PASSWORD', '$NNTP_PASSWORD');" >> /config/config.php
+	echo "define('NNTP_SERVER', '$NNTP_SERVER');" >> /config/config.php
+	echo "define('NNTP_PORT', '$NNTP_PORT');" >> /config/config.php
+	echo "define('NNTP_SSLENABLED', $NNTP_SSLENABLED);" >> /config/config.php
 fi
 
 if [ -f /config/config.php ]; then
