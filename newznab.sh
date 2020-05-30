f035a0cf637779cf6fec714eea2acffebe7b88fe
fekhoo <44518355+fekhoo@users.noreply.github.com>
2020-05-30 13:52:41 -0400

    Update newznab.sh

--- newznab.sh
+++ newznab.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
-# call this script from within screen to get binaries, processes releases and
-# every half day get tv/theatre info and optimise the database
 
-set -e
+#Data Base 
+sed -i "s/'mysql'/'$DB_TYPE'/" /var/www/newznab/www/config.php
+sed -i "s/'localhost'/'$DB_HOST'/" /var/www/newznab/www/config.php
+sed -i "s/3306/$DB_PORT/" /var/www/newznab/www/config.php
+sed -i "s/'root'/'$DB_USER'/" /var/www/newznab/www/config.php
+sed -i "s/'password'/'$DB_PASSWORD'/" /var/www/newznab/www/config.php
+sed -i "s/'newznab'/'$DB_NAME'/" /var/www/newznab/www/config.php
+
+#Usenet Server
+sed -i "s/'nnuser'/'$NNTP_USERNAME'/" /var/www/newznab/www/config.php
+sed -i "s/'nnpass'/'$NNTP_PASSWORD'/" /var/www/newznab/www/config.php
+sed -i "s/'nnserver'/'$NNTP_SERVER'/" /var/www/newznab/www/config.php
+sed -i "s/563/$NNTP_PORT/" /var/www/newznab/www/config.php
+sed -i "s/'NNTP_SSLENABLED', true/'NNTP_SSLENABLED', $NNTP_SSLENABLED/" /var/www/newznab/www/config.php
+
 
   export NEWZNAB_PATH="/var/www/newznab/misc/update_scripts"
   export NEWZNAB_SLEEP_TIME="10" # in seconds
