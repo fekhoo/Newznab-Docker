a7435e01b0bfc894680e5e637e96ffb2f20c59de
fekhoo <44518355+fekhoo@users.noreply.github.com>
2020-05-31 18:39:11 -0400

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,21 @@
+# Update php.ini file
+RUN sed -i "s/max_execution_time = 30/max_execution_time = 120/" /etc/php/7.4/fpm/php.ini  && \
+    echo "date.timezone = $TZ" >> /etc/php/7.4/fpm/php.ini && \
+    sed -i "s/max_execution_time = 30/max_execution_time = 120/" /etc/php/7.4/cli/php.ini  && \
+    echo "date.timezone = $TZ" >> /etc/php/7.4/cli/php.ini  && \  
+    sed -i "s/max_execution_time = 30/max_execution_time = 120/" /etc/php/7.4/apache2/php.ini  && \
+    sed -i "s/memory_limit = 128M/memory_limit = -1/" /etc/php/7.4/apache2/php.ini  && \
+    echo "date.timezone = $TZ" >> /etc/php/7.4/apache2/php.ini
+    
+# Configure Apache for Newznab site
+COPY newznab.conf /etc/apache2/sites-available/newznab.conf
+RUN a2dissite 000-default.conf && \
+    a2ensite newznab 
+    
+# Enable apache mod_rewrite, fpm and restart services
+RUN a2enmod proxy_fcgi setenvif && \
+    a2enconf php7.4-fpm && \
+    a2enmod rewrite && \
+    service php7.4-fpm reload && \
+    service apache2 restart
+    
