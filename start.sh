989ea248e7f3b80b9c4849ce89851eaa3ec43824
fekhoo <44518355+fekhoo@users.noreply.github.com>
2020-06-01 00:46:08 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -70,8 +70,11 @@ echo "date.timezone = $TZ" >> /etc/php/7.4/cli/php.ini
 sed -i "s/max_execution_time = 30/max_execution_time = 120/" /etc/php/7.4/apache2/php.ini
 sed -i "s/memory_limit = 128M/memory_limit = -1/" /etc/php/7.4/apache2/php.ini
 echo "date.timezone = $TZ" >> /etc/php/7.4/apache2/php.ini
-      
+
+
+
 # Enable apache mod_rewrite, fpm and restart services
+rm -rf /run/apache2/apache2.pid
 a2enmod proxy_fcgi setenvif
 a2enconf php7.4-fpm
 a2enmod rewrite
