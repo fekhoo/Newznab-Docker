93232a5796ee266799ea28bd2a04135abaf28079
fekhoo <44518355+fekhoo@users.noreply.github.com>
2020-06-01 02:21:55 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,87 @@
+#!/usr/bin/env bash
 
 # Update php.ini file
-RUN sed -i "s/max_execution_time = 30/max_execution_time = 120/" /etc/php/7.4/fpm/php.ini && \
-    echo "date.timezone = $TZ" >> /etc/php/7.4/fpm/php.ini && \
-    sed -i "s/max_execution_time = 30/max_execution_time = 120/" /etc/php/7.4/cli/php.ini && \
-    echo "date.timezone = $TZ" >> /etc/php/7.4/cli/php.ini && \
-    sed -i "s/max_execution_time = 30/max_execution_time = 120/" /etc/php/7.4/apache2/php.ini && \
-    sed -i "s/memory_limit = 128M/memory_limit = -1/" /etc/php/7.4/apache2/php.ini && \
-    echo "date.timezone = $TZ" >> /etc/php/7.4/apache2/php.ini
+sed -i "s/max_execution_time = 30/max_execution_time = 120/" /etc/php/7.4/fpm/php.ini
+echo "date.timezone = $TZ" >> /etc/php/7.4/fpm/php.ini
+sed -i "s/max_execution_time = 30/max_execution_time = 120/" /etc/php/7.4/cli/php.ini
+echo "date.timezone = $TZ" >> /etc/php/7.4/cli/php.ini
+sed -i "s/max_execution_time = 30/max_execution_time = 120/" /etc/php/7.4/apache2/php.ini
+sed -i "s/memory_limit = 128M/memory_limit = -1/" /etc/php/7.4/apache2/php.ini
+echo "date.timezone = $TZ" >> /etc/php/7.4/apache2/php.ini
     
 # Enable apache mod_rewrite, fpm and restart services
-RUN a2enmod proxy_fcgi setenvif && \
-    a2enconf php7.4-fpm && \
-    a2enmod rewrite && \
-    service php7.4-fpm reload && \
-    service apache2 restart
+a2enmod proxy_fcgi setenvif
+a2enconf php7.4-fpm
+a2enmod rewrite
+service php7.4-fpm reload
+
+#Creating needed folders
+if [ ! -f /var/www/newznab/www/covers/anime ]; then 
+mkdir -p /var/www/newznab/www/covers/anime
+chmod  -R 777 /var/www/newznab/www/covers/anime
+fi
+
+if [ ! -f /var/www/newznab/www/covers/music ]; then
+mkdir -p /var/www/newznab/www/covers/music
+chmod  -R 777 /var/www/newznab/www/covers/music
+fi
+
+if [ ! -f /var/www/newznab/www/covers/tv ]; then
+mkdir -p /var/www/newznab/www/covers/tv
+chmod  -R 777 /var/www/newznab/www/covers/tv
+fi
+
+if [ ! -f /var/www/newznab/www/covers/audio ]; then
+mkdir -p /var/www/newznab/www/covers/audio
+chmod  -R 777 /var/www/newznab/www/covers/audio
+fi
+ 
+if [ ! -f /var/www/newznab/www/covers/book ]; then
+mkdir -p /var/www/newznab/www/covers/book
+chmod  -R 777 /var/www/newznab/www/covers/book
+fi
+
+if [ ! -f /var/www/newznab/www/covers/console ]; then
+mkdir -p /var/www/newznab/www/covers/console
+chmod  -R 777 /var/www/newznab/www/covers/console
+fi
+
+if [ ! -f /var/www/newznab/www/covers/movies ]; then
+mkdir -p /var/www/newznab/www/covers/movies
+chmod  -R 777 /var/www/newznab/www/covers/movies
+fi
+
+if [ ! -f /var/www/newznab/www/covers/preview ]; then
+mkdir -p /var/www/newznab/www/covers/preview
+chmod  -R 777 /var/www/newznab/www/covers/preview
+fi
+
+# Edit config file DataBase settings
+sed -i "s/'mysql'/'$DB_TYPE'/" /var/www/newznab/www/config.php
+sed -i "s/'localhost'/'$DB_HOST'/" /var/www/newznab/www/config.php
+sed -i "s/3306/$DB_PORT/" /var/www/newznab/www/config.php
+sed -i "s/'root'/'$DB_USER'/" /var/www/newznab/www/config.php
+sed -i "s/'password'/'$DB_PASSWORD'/" /var/www/newznab/www/config.php
+sed -i "s/'newznab'/'$DB_NAME'/" /var/www/newznab/www/config.php
+
+#Edit config file Usenet Server Settings
+sed -i "s/'nnuser'/'$NNTP_USERNAME'/" /var/www/newznab/www/config.php
+sed -i "s/'nnpass'/'$NNTP_PASSWORD'/" /var/www/newznab/www/config.php
+sed -i "s/'nnserver'/'$NNTP_SERVER'/" /var/www/newznab/www/config.php
+sed -i "s/563/$NNTP_PORT/" /var/www/newznab/www/config.php
+sed -i "s/'NNTP_SSLENABLED', true/'NNTP_SSLENABLED', $NNTP_SSLENABLED/" /var/www/newznab/www/config.php
+
+# Getting script ready for newznab Screen
+cp /var/www/newznab/misc/update_scripts/nix_scripts/newznab_screen.sh /var/www/newznab/misc/update_scripts/nix_scripts/newznab_local.sh
+sed -i "s|/var/www/newznab/htdocs/misc/update_scripts|/var/www/newznab/misc/update_scripts|" /var/www/newznab/misc/update_scripts/nix_scripts/newznab_local.sh
+sed -i "s|30|10|" /var/www/newznab/misc/update_scripts/nix_scripts/newznab_local.sh
+chmod a+x /var/www/newznab/misc/update_scripts/nix_scripts/newznab_local.sh
+
+# Restart Apache2
+/etc/init.d/apache2 restart
+
+# Start newznab Service
+/etc/init.d/autostart.sh start
+
+#Keep Docker up!!
+tail -f /var/log/apache2/* /dev/stdout /dev/stderr
