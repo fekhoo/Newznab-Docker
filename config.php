847e18d4c117128ef514c84f4fd02cea6755ece8
fekhoo <44518355+fekhoo@users.noreply.github.com>
2020-06-01 15:56:38 -0400

    Update config.php

--- config.php
+++ config.php
@@ -23,7 +23,7 @@ define('CACHEOPT_METHOD', 'none');
 define('CACHEOPT_TTLFAST', '120');
 define('CACHEOPT_TTLMEDIUM', '600');
 define('CACHEOPT_TTLSLOW', '1800');
-define('CACHEOPT_MEMCACHE_SERVER', '127.0.0.1');
+define('CACHEOPT_MEMCACHE_SERVER', 'memserver');
 define('CACHEOPT_MEMCACHE_PORT', '11211');
 
 // define('EXTERNAL_PROXY_IP', ''); //Internal address of public facing server
