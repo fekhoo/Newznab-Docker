29152a508692a020af5bc595474a8222087c9515
fekhoo <44518355+fekhoo@users.noreply.github.com>
2020-06-01 01:00:37 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -62,22 +62,5 @@ sed -i "s|/var/www/newznab/htdocs/misc/update_scripts|/var/www/newznab/misc/upda
 sed -i "s|30|10|" /var/www/newznab/misc/update_scripts/nix_scripts/newznab_local.sh
 chmod a+x /var/www/newznab/misc/update_scripts/nix_scripts/newznab_local.sh
 
-# Update php.ini file
-sed -i "s/max_execution_time = 30/max_execution_time = 120/" /etc/php/7.4/fpm/php.ini
-echo "date.timezone = $TZ" >> /etc/php/7.4/fpm/php.ini
-sed -i "s/max_execution_time = 30/max_execution_time = 120/" /etc/php/7.4/cli/php.ini
-echo "date.timezone = $TZ" >> /etc/php/7.4/cli/php.ini
-sed -i "s/max_execution_time = 30/max_execution_time = 120/" /etc/php/7.4/apache2/php.ini
-sed -i "s/memory_limit = 128M/memory_limit = -1/" /etc/php/7.4/apache2/php.ini
-echo "date.timezone = $TZ" >> /etc/php/7.4/apache2/php.ini
-
-
-
-# Enable apache mod_rewrite, fpm and restart services
-a2enmod proxy_fcgi setenvif
-a2enconf php7.4-fpm
-a2enmod rewrite
-service php7.4-fpm reload
-service apache2 restart
 
 /bin/bash -c "source /etc/apache2/envvars && exec /usr/sbin/apache2 -DFOREGROUND"
